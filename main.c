53ec7663e789a2866a9bd2e61b0bf9b41a41f329
Napster777 <jorge.zapata@protonmail.com>
2020-05-24 12:22:31 -0600

    tSerial

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 double *tSerial(double n[7]){
     static double result[7];
 
-    for (int i = 0; i <= 7; ++i){
+    for (int i = 0; i <= sizeof(result); ++i){
         result[i] = pow(n[i], 2.0);
     }
 
-    for(int j = 0; j <= 7; ++j){
+    for(int j = 0; j <= sizeof(result); ++j){
         printf("%f\n", result[j]);
     }
 
@@ -20,9 +20,11 @@ int main(int argc,  char * argv[]) {
     double n[7] = {10.0, 20.0, 30.0, 40.0, 80.0, 160.0, 320.0};
     double p[8] = {1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0};
 
-    double * tSerialResult = tSerial(n);
+    double *tSerialResult = tSerial(n);
 
-
+    for (int i=0; i < sizeof(tSerialResult); ++i){
+        printf("%f\n", tSerialResult[i]);
+    }
 
     return 0;
 }
