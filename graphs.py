e98102d2ada1cda0182e9553507a59f0771f86b3
Napster777 <jorge.zapata@protonmail.com>
2020-05-26 16:13:13 -0600

    actualizado

--- graphs.py
+++ graphs.py
@@ -1,19 +1,7 @@
 import pandas as pd
 import seaborn as sns
 
-workers = {'workers':[1, 2, 4, 8, 16, 32, 64, 128]}
-df_speedup = pd.read_csv("files/speedup.txt", sep=" ", names=["iteration", "speedup"])
-df_efficiency = pd.read_csv("files/efficiency.txt", sep=" ", names=["iteration", "efficiency"])
+df = pd.read_csv("files/data.txt", sep=",")
+print(df)
 
-df_pworkers = pd.DataFrame(workers)
-
-df = pd.concat([df_pworkers, df_speedup["speedup"], df_efficiency["efficiency"]], axis=1)
-
-sns.set()
-g1 = sns.lineplot(x="workers", y="speedup", data=df)
-fig = g1.get_figure()
-fig.savefig("images/speedup.png")
-
-g2 = sns.lineplot(x="workers", y="efficiency", data=df)
-fig2 = g2.get_figure()
-fig2.savefig("images/efficiency.png")
+print("Successfully export")
--- main.c
+++ main.c
@@ -1,110 +1,35 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-double *tSerial(double n[7]){
-    static double resultSerial[7];
+#define MAX_LINE 100
 
-    for (int i = 0; i <= 7; ++i){
-        resultSerial[i] = pow(n[i], 2.0);
+void *results(){
+    double n[6] = {10.0, 20.0, 40.0, 80.0, 160.0, 320.0};
+    double p[8] = {1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0};
+
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            double tserial = pow(n[i], 2.0);
+            double tparallel = (pow(n[i], 2.0) / p[j]) + log2(p[j]);
+            double speedup = tserial/tparallel;
+            double effi = speedup/p[j];
+
+
+            if (j == 0 && i == 0){
+                printf("n,p,speedup,efficiency\n%f,%f,%f,%f\n", n[i], p[j], speedup, effi);
+            } else {
+                printf("%f,%f,%f,%f\n", n[i], p[j], speedup, effi);
+            }
+        }
     }
-
-    return resultSerial;
-}
-
-double *tParallel(double p[8]){
-    static double resultParallel[8];
-
-    for (int i = 0; i <= 8; ++i){
-        resultParallel[i] = pow(p[i], 2.0) / (pow(p[i], 2.0) / p[i] + log2(p[i]));
-    }
-
-    return resultParallel;
-}
-
-double *speedup(double *serial, double *parallel){
-    static double resultSpeedup[8];
-
-    for (int i = 0; i <= 8; ++i){
-        resultSpeedup[i] = serial[i] / parallel[i];
-    }
-}
-
-double *efficiency(double *speedup, double *p){
-    static double resultEfficiency[8];
-
-    for (int i = 0; i <= 8; ++i){
-        resultEfficiency[i] = speedup[i] / p[i];
-    }
-}
-
-void *printResults(double *serial, double *parallel, double *speedup, double *efficiency){
-    printf("******** Serial Result *********\n");
-
-    for (int i=0; i < 7; ++i){
-        printf("%d %f\n", i+1, serial[i]);
-    }
-
-    printf("\n******** Parallel Result ***********\n");
-
-    for (int i=0; i < 8; ++i){
-        printf("%d %f\n", i+1, parallel[i]);
-    }
-
-    printf("\n******** Speedup ***********\n");
-
-    for (int i=0; i < 8; ++i){
-        printf("%d %f\n", i+1, speedup[i]);
-    }
-
-    printf("\n******** Efficiency ***********\n");
-
-    for (int i=0; i < 8; ++i){
-        printf("%d %f\n", i+1, efficiency[i]);
-    }
-
     return 0;
 }
 
-/*
-void * createFile(tSerial, tParallel){
-    FILE *fps = fopen("serial.txt", "w");
-
-    for (int i = 0; i < 7; ++i){
-        fprintf(fps, tSerial[i]);
-    }
-
-    fclose(fps);
-
-    FILE *fpp = fopen("parallel.txt", "w");
-    fprintf(fpp, tParallel);
-    fclose(fpp);
-    return 0;
-}
-*/
 
 int main(int argc,  char * argv[]) {
     //Napster
-    double n[7] = {10.0, 20.0, 30.0, 40.0, 80.0, 160.0, 320.0};
-    double p[8] = {1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0};
-
-    double *tSerialResult = tSerial(n);
-    double *tParallelResult = tParallel(p);
-    double *speedupResult = speedup(tSerialResult, tParallelResult);
-    double *efficiencyResult = efficiency(speedupResult, p);
-
-    printResults(tSerialResult, tParallelResult, speedupResult, efficiencyResult);
-
-/*
-    createFile(tSerialResult, tParallelResult);
-
-    FILE *fps = fopen("serial.txt", "w");
-    char serialArray[100] = "";
-
-    for (int i = 0; i < 7; ++i){
-        serialArray[i] = tSerialResult[i];
-        printf("Serial %c", serialArray[i]);
-    }
-*/
-
+    results();
     return 0;
 }
